45ab23165bb816e842f31a1166f60e15246fa510
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2020-12-14 20:23:16 +0100

    chore: added more tests, closes #17

--- test/test_oximachine.py
+++ test/test_oximachine.py
@@ -79,3 +79,16 @@ def test_oximachine():
         os.path.join(THIS_DIR, "..", "examples/IDIWIB_clean.cif")
     )
     assert output["prediction"] == [3, 3, 3, 3]
+
+    output = runner.run_oximachine(
+        os.path.join(THIS_DIR, "..", "examples/IDIWOH_clean.cif")
+    )
+    assert output["prediction"] == [4, 4, 4, 4]
+
+    output = runner.run_oximachine(
+        os.path.join(THIS_DIR, "..", "oximachinerunner/assets/ACODAA.cif")
+    )
+    assert len(output) == 5
+    assert output["prediction"] == [2, 2]
+    assert output["metal_indices"] == [0, 1]
+    assert output["metal_symbols"] == ["Fe", "Fe"]
